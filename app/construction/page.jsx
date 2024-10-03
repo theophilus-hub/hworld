2189cb0eddd05caf4e2b5d4f6135a233703d0016
web design <wdesign589@gmail.com>
2024-10-03 20:34:30 +0100

    fixed build error

---
+++ app/construction/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Construction() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Construction
---
+++ app/marine/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Marine() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Marine
